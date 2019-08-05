6057715182f326af5db01ac3dc5029fe4ea26ba2
paulaan <paul.nguyen@mobiclixgroup.com>
2019-08-05 18:18:19 +0700

    [master] Refine Demo sample

--- examples/main.go
+++ examples/main.go
@@ -1,26 +1,31 @@
 package main
 
-import "github.com/sakari-ai/analytics-go"
+import (
+	"context"
+	"github.com/sakari-ai/analytics-go"
+)
 
 // User mock up sourcing
 // Ideally, this UserSession service must return user ID via Context
 type UserSession struct {
 }
 
-func (u *UserSession) GetUserId() string {
-	// return your current user id by session base
+//GetUserId will rely user-id in context
+func (u *UserSession) GetUserId(ctx context.Context) string {
 	return "user-id"
 }
+
 func main() {
 	session := new(UserSession)
 	sakari := analytics.New("uyKDw5O0p0t6IwuWAMtiMixT2vXpoLUH",
 		"156c5bcd-2b8b-437c-b58c-10ab2982cc28",
-		analytics.WithInitialUserSourcing(session.GetUserId))
+		analytics.WithInitialUserSourcing(session))
 
 	defer sakari.Close()
 
 	trackPage(sakari)
 	trackArbitraryEvent(sakari)
+	trackGroupEvent_By_Context(sakari)
 }
 
 func trackPage(sakari *analytics.Client) {
@@ -32,6 +37,9 @@ func trackArbitraryEvent(sakari *analytics.Client) {
 		Properties: analytics.NewProperties().Set("name", "Paul").Set("duration", 10)})
 }
 
-func trackGroupEvent(sakari *analytics.Client) {
-	_ = sakari.Group(&analytics.Group{GroupId: "group-1", Traits: analytics.NewTrait().Set("package", "premium")})
+func trackGroupEvent_By_Context(sakari *analytics.Client) {
+	_ = sakari.Group(
+		&analytics.Group{GroupId: "group-1", Traits: analytics.NewTrait().Set("package", "premium")},
+		analytics.WithContext(context.Background()), // optional
+	)
 }
